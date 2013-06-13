c0f964856bc8dee211551f57a616a240d3374bab
Steven Berlanga <steven.berlanga@cloud.com>
2013-06-12 23:30:48 -0400

    Add javadoc to project

--- android/src/cucumber/api/android/RunWithCucumber.java
+++ android/src/cucumber/api/android/RunWithCucumber.java
@@ -8,7 +8,21 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE})
 public @interface RunWithCucumber {
+	/**
+	 * 
+	 * @return default is " "
+	 */
     String glue() default "";
 
+    /**
+     * Set the path to the feature files that you want to run. Path must be relative to the assets folder
+     * @return default is "features"
+     */
     String features() default "features";
+    
+    /**
+     * Set the tags that will filter out and run specific feature files.
+     * @return default is " "
+     */
+    String[] tags() default { };
 }
