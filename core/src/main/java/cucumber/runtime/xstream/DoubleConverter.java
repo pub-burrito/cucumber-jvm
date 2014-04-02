28a76edfef4b62f46d8ca79343086ac238730342
Roberto Andrade <roberto.andrade@gmail.com>
2014-04-02 18:20:17 -0400

    Considering scientific notation number format during parsing

--- core/src/main/java/cucumber/runtime/xstream/DoubleConverter.java
+++ core/src/main/java/cucumber/runtime/xstream/DoubleConverter.java
@@ -1,11 +1,20 @@
 package cucumber.runtime.xstream;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.Locale;
 
 class DoubleConverter extends ConverterWithNumberFormat<Double> {
 
     public DoubleConverter(Locale locale) {
         super(locale, new Class[]{Double.class, Double.TYPE});
+        
+        DecimalFormatSymbols symbols = DecimalFormatSymbols.getInstance( Locale.US );
+		symbols.setExponentSeparator( "e" );
+		
+		DecimalFormat scientificNotificationAwareDecimalFormat = new DecimalFormat("#.#", symbols);
+		
+        getFormats().add( 0, scientificNotificationAwareDecimalFormat );
     }
 
     @Override
